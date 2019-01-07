2fe07a6415928f7261cca55cc84b530c89853d86
zalopix <marry.jane.kelly@gmail.com>
2019-01-07 17:21:46 +0100

    hoc stateful component example

--- first-app/src/hoc/withClass1.js
+++ first-app/src/hoc/withClass1.js
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 //normal javascript function that qualifies as hoc
-//{...props} will send all of the props without any modification  :D simple as that 
+//{...props} will send all of the props without any modification  :D simple as that
 
-
+/*
 const withClass1 = (WrappedComponent, className) => {
     return (props) => (
         <div className={className}>
             <WrappedComponent  {...props}/>
         </div>
     );
+}
+*/
+
+
+//similar to above but stateful component
+
+const withClass1 = (WrappedComponent, className) => {
+    return class extends Component {
+        render() {
+            return (
+                <div className={className}>
+                    <WrappedComponent  {...this.props}/>
+                </div>
+            )
+        }
+    }
 }
 
 
